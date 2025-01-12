9b9e009aa811eb766e8c472a0b193070e20c576e
Jason Coawette <jason.coawette@gmail.com>
2025-01-12 13:15:54 -0700

    Fixed Decrypting text from moving shit around

--- app/components/landing/DecryptingText.tsx
+++ app/components/landing/DecryptingText.tsx
@@ -1,20 +1,27 @@
-'use client';
+"use client";
 
+import { useState, useEffect } from 'react';
 import useEncryptText from '@/app/hooks/useEncryptText';
 
-// can we make sure this loads with the page so it doesnt shift the text wrapper up
 type DecryptingTextProps = {
     text: string;
 };
 
 export default function DecryptingText({ text }: DecryptingTextProps) {
     const { displayText } = useEncryptText(text);
-    
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 0); 
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <h4 className="
-            TagText
-        ">
-            {displayText}
+        <h4 className="TagText">
+            {isLoaded ? displayText : <span className="invisible">{text}</span>}
         </h4>
     );
 };
