4630e70ab17aad7931b844b22f712cc47885c44b
Jason Coawette <jason.coawette@gmail.com>
2025-01-13 23:00:48 -0700

    Dashboard deleted gonna restart after auth

--- app/components/dashboard/unviersal/H2.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from "@/utils/utils";
-
-export default function H2({ children, className }: { children: React.ReactNode; className?: string }) {
-  return <h2 className={cn("font-sans text-[22px] subpixel-antialiased font-semibold tracking-[-0.02em] leading-none text-primaryFG", className)}>{children}</h2>;
-}
